e67a4d6c9cc54a462a459f214b851215d4b56739
rong <tairong_kang@innodisk.com>
2021-01-07 10:54:26 +0800

    feat: show number of publish data

--- device/client.js
+++ device/client.js
@@ -10,20 +10,23 @@ const {
 } = require('./mqttConnector');
 const topic = require('../constant/mqttTopic');
 
+const timeArr = [];
+
 function publishData(frequency) {
-  deviceList.forEach(device => {
+  deviceList.forEach((device, idx) => {
     console.log("device info: ", device);
-    let t = 1;
+    timeArr[idx] = 0;
     const client = initConnect(device);
 
     const timeId = setInterval(() => {
-      if ((t * frequency) >= testTime && +testTime !== 0) {
+      if ((timeArr[idx] * frequency) >= testTime && +testTime !== 0) {
         clearInterval(timeId);
         console.log('test end');
       }
       client.publish(topic, JSON.stringify(rawData()))
-      console.log(`${device.name} connected`, t++);
+      console.log(`${device.name} connected`, timeArr[idx] += 1);
       if (client.disconnected) clearInterval(timeId);
+      if (idx === timeArr.length - 1) console.log('total', timeArr.reduce((accu, curr) => accu + curr));
     }, frequency * 1000);
   });
 }

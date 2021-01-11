e624e3cd2f61c0332efa09fe571b0baba5c2781e
rong <tairong_kang@innodisk.com>
2021-01-11 16:07:07 +0800

    feat: change payload

--- modules/index.js
+++ modules/index.js
@@ -3,7 +3,35 @@ function randonNum(params) {
 }
 function rawData() {
   return {
-    "CPU.Usage": randonNum(100)
+    "CPU": {
+      "0": {
+        "Freq": 1003,
+        "Usage": 68.0,
+        "Temp": 44,
+        "V": 8
+      },
+      "1": {
+        "Freq": 1387,
+        "Usage": 89.0,
+        "Temp": 43,
+        "V": 10
+      },
+      "2": {
+        "Freq": 1261,
+        "Usage": 54.0,
+        "Temp": 20,
+        "V": 9
+      },
+      "3": {
+        "Freq": 1366,
+        "Usage": 16.0,
+        "Temp": 24,
+        "V": 8
+      },
+      "Freq": 2271,
+      "Usage": 61.0,
+      "FanRPM": 1971
+    }
   };
 }
 function requestOpt(raw) {
